2746ab9b23fced4e7a1dc746f027bb5cec124722
Sebastian Bugge <vkbugge@hotmail.com>
2024-01-22 01:47:50 +0100

    Add custom plugins.

---
+++ lua/plugins/navigator.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "numToStr/Navigator.nvim",
+    lazy = false,
+    keys = {
+      { "<C-h>", "<cmd>NavigatorLeft<cr>", desc = "Move to left TMUX pane." },
+      { "<C-j>", "<cmd>NavigatorDown<cr>", desc = "Move lo lower TMUX pane." },
+      { "<C-k>", "<cmd>NavigatorUp<cr>", desc = "Move to upper TMUX pane." },
+      { "<C-l>", "<cmd>NavigatorRight<cr>", desc = "Move to right TMUX pane." },
+    },
+    opts = {
+      auto_save = "all",
+      mux = "auto",
+    },
+  },
+}
